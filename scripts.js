ef9b29d913d601bb636858c0958c061fbf879fb0
AntonioValgreen <61595675+AntonioValgreen@users.noreply.github.com>
2020-02-28 10:54:08 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function mostrarFoto7(){
+document.getElementById("foto").style.display = "block";
+}
+
+function ocultarFoto7(){
+document.getElementById("foto").style.display = "none";
+}
+                 
+function validarTelefono7(){
+
+                          }
+function encontrarLetra7(){
+    
+}
